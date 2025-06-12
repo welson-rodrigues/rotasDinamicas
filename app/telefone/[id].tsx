829f16770c0804541e88e1e5d6928c2c57953297
Aluno <aluno@debian.ifrn.local>
2025-06-12 14:26:49 -0300

    Fazendo

--- app/telefone/[id].tsx
+++ app/telefone/[id].tsx
@@ -3,8 +3,14 @@ import { useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 
+type ItemType = {
+  id: string;
+  name: string;
+  data: Record<string, any>;
+};
+
 // começando por aqui
-const globalData = {
+const globalData = ItemType[] = Array.from({ length: 10 }, (_, i) =>{
     id: `${i}`,
     name: `Celular ${i + 1}`,
     data: {
@@ -14,7 +20,10 @@ const globalData = {
       generation: `Geração ${i + 1}`,
       year: 2023,
     },
-}
+}),
+
+const [data, setData] = useState<ItemType[]>([]);
+const [isLoading, setIsLoading] = useState(true);
 
 export default function TelefoneDetalhe() {
   const { id } = useLocalSearchParams();
