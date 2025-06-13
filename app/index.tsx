251f4d4d019d5c601ee5977ba917659d2853dbeb
Aluno <aluno@debian.ifrn.local>
2025-06-13 13:42:36 -0300

    Consumindo API

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { Link } from "expo-router";
 import { useEffect, useState } from 'react';
 import { Dimensions, FlatList, StyleSheet, View } from "react-native";
 import { Card, Text } from "react-native-paper";
+import { ItemType } from '../types';
 
 // ajusta as dimensões do card para o tamanho da tela
 // não esqueça de dar um F5 no navegador para ver as mudanças
@@ -9,39 +11,20 @@ const CARD_MARGIN = 8;
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const CARD_WIDTH = SCREEN_WIDTH / 2 - CARD_MARGIN * 3;
 
-type ItemType = {
-  id: string;
-  name: string;
-  data: Record<string, any>;
-};
-
-// Exemplo de dados para o FlatList
-// precisamos converter isso em uma chamada a uma API (https://api.restful-api.dev/objects)
-// removeremos isso após programarmos a chamada à API
-const globalData: ItemType[] = Array.from({ length: 10 }, (_, i) => ({
-  id: `${i}`,
-  name: `Celular ${i + 1}`,
-  data: {
-    color: `Cor ${i + 1}`,
-    capacity: `Capacidade ${i + 1}`,
-    price: `$${(i + 1) * 100}`,
-    generation: `Geração ${i + 1}`,
-    year: 2023,
-  },
-}));
-
 export default function Index() {
 
   const [data, setData] = useState<ItemType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  async function buscarDados() {
+    setIsLoading(true)
+    const resultado = await axios.get('https://api.restful-api.dev/objects')
+    setData(resultado.data)
+    setIsLoading(false)
+  }
+
   useEffect(() => {
-    setTimeout(() => {
-      // Simula uma chamada a uma API
-      setData(globalData);
-      setIsLoading(false);
-    },
-    3000); //simula um atraso de 3 segundos
+      buscarDados()
   },[])
 
   const renderItem = ({ item }: { item: ItemType }) => (
