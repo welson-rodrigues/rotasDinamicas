1eb8807e93a3340d8bd771848f43d0fbac2d8cf3
Welson Rosendo Rodrigues <164583871+welson-rodrigues@users.noreply.github.com>
2025-06-23 00:05:54 +0000

    Buscando o restante dos dados do telefone

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 import { Link } from "expo-router";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Dimensions, FlatList, StyleSheet, View } from "react-native";
 import { Card, Text } from "react-native-paper";
-import { ItemType } from '../types';
+import { ItemType } from "../types";
 
 // ajusta as dimensões do card para o tamanho da tela
 // não esqueça de dar um F5 no navegador para ver as mudanças
@@ -12,26 +12,28 @@ const SCREEN_WIDTH = Dimensions.get("window").width;
 const CARD_WIDTH = SCREEN_WIDTH / 2 - CARD_MARGIN * 3;
 
 export default function Index() {
-
   const [data, setData] = useState<ItemType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   async function buscarDados() {
-    setIsLoading(true)
-    const resultado = await axios.get('https://api.restful-api.dev/objects')
-    setData(resultado.data)
-    setIsLoading(false)
+    setIsLoading(true);
+    const resultado = await axios.get("https://api.restful-api.dev/objects");
+    setData(resultado.data);
+    setIsLoading(false);
   }
 
   useEffect(() => {
-      buscarDados()
-  },[])
+    buscarDados();
+  }, []);
 
   const renderItem = ({ item }: { item: ItemType }) => (
-    <Link href={{
-      pathname: '/telefone/[id]',
-      params: { id: item.id}
-      }} style={{ flex: 1 }}>
+    <Link
+      href={{
+        pathname: "/telefone/[id]",
+        params: { id: item.id },
+      }}
+      style={{ flex: 1 }}
+    >
       <Card style={styles.card}>
         <Card.Content>
           <Text variant="titleMedium">{item.name}</Text>
@@ -62,8 +64,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   carregando: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     padding: CARD_MARGIN,
--- app/telefone/[id].tsx
+++ app/telefone/[id].tsx
@@ -1,12 +1,28 @@
-import { useLocalSearchParams, useNavigation } from 'expo-router';
-import { useLayoutEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import axios from "axios";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useLayoutEffect, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
+
 
 export default function TelefoneDetalhe() {
   const { id } = useLocalSearchParams();
 
+  const [telefone, setTelefone] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   const navigation = useNavigation();
+
+  async function buscarTelefone() {
+    setIsLoading(true);
+    const resultado = await axios.get(`https://api.restful-api.dev/objects/${id}`);
+    setTelefone(resultado.data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    buscarTelefone();
+  }, [id]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -14,9 +30,23 @@ export default function TelefoneDetalhe() {
     });
   }, [navigation, id]);
 
+  if (isLoading) {
+    return (
+      <View style={styles.carregando}>
+        <Text variant="bodyLarge">Carregando...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text variant="bodyLarge">ID: {id}</Text>
+      <Text variant="bodyLarge">Color: {telefone.data.color}</Text>
+      <Text variant="bodyLarge">Capacity: {telefone.data.capacity}</Text>
+      <Text variant="bodyLarge">Price: {telefone.data.price}</Text>
+      <Text variant="bodyLarge">Generation: {telefone.data.generation}</Text>
+      <Text variant="bodyLarge">Year: {telefone.data.year}</Text>
+      <Text variant="bodyLarge">Description: {telefone.data.Description}</Text>
     </View>
   );
 }
@@ -25,6 +55,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
+  },
+
+  carregando: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
