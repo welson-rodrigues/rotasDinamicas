a528f3c18d4c42e9e8144c51c2467a55b08b340d
Welson Rosendo Rodrigues <164583871+welson-rodrigues@users.noreply.github.com>
2025-06-23 01:07:41 +0000

    Estilizando

--- app/telefone/[id].tsx
+++ app/telefone/[id].tsx
@@ -46,51 +46,63 @@ export default function TelefoneDetalhe() {
     <View style={styles.container}>
       <Text variant="bodyLarge">ID: {id}</Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.color}>
         Color: {telefone.data ? (telefone.data.color || telefone.data.Color || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Capacity}>
         Capacity: {telefone.data ? (telefone.data.capacity || telefone.data.Capacity || telefone.data["capacity GB"] || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Price}>
         Price: {telefone.data ? (telefone.data.price || telefone.data.Price || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Generation}>
         Generation: {telefone.data ? (telefone.data.generation || telefone.data.Generation || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Year}>
         Year: {telefone.data ? (telefone.data.year || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Description}>
         Description: {telefone.data ? (telefone.data.Description || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+      style={styles.CaseSize}>
         Case Size: {telefone.data ? (telefone.data["Case Size"] || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.CPUmodel}>
         CPU model: {telefone.data ? (telefone.data["CPU model"] || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Harddisksize}>
         Hard disk size: {telefone.data ? (telefone.data["Hard disk size"] || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.StrapColour}>
         Strap Colour: {telefone.data ? (telefone.data["Strap Colour"] || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.Screensize}>
         Screen size: {telefone.data ? (telefone.data["Screen size"] || "null") : "null"}
       </Text>
 
-      <Text variant="bodyLarge">
+      <Text variant="bodyLarge"
+        style={styles.CaseSize}>
         Case Size: {telefone.data ? (telefone.data["Case Size"] || "null") : "null"}
       </Text>
     </View>
@@ -108,5 +120,49 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  color: {
+    color: "red",
+  },
+
+  Capacity: {
+    color: "blue",
+  },
+
+  Price: {
+    color: "green",
+  },
+
+  Generation: {
+    color: "purple",
+  },
+
+  Year: {
+    color: "orange",
+  },
+
+  Description: {
+    color: "brown",
+  },
+
+  CaseSize: {
+    color: "pink",
+  },
+
+  CPUmodel: {
+    color: "gray",
+  },
+
+  Harddisksize: {
+    color: "black",
+  },
+
+  StrapColour: {
+    color: "yellow",
+  },
+
+  Screensize: {
+    color: "cyan",
   },
 });
